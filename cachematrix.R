e2dd29c695138b445ded48037c479c6e7138759e
Buddy <Buddy@Gunter.local>
2015-01-25 15:28:15 -0500

    Finished assignment 2
    Booya!

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,32 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This funky function creates a matrix that will cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
 ## Write a short comment describing this function
-
+## This function takes the matrix created by his buddy makeCacheMatrix and figures out the inverse.
+## If the inverse has already been calculated, then he he will go get it. He's cool like that.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
